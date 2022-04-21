0ad665eb891ee18bf267369ffc09d804c763cdff
joilsonlira <joilson.lira@live.com>
2022-04-21 16:56:02 -0300

    adicionado a variavel port para deploy no heroku

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const server = express()
 const route = require('./route')
 const path = require('path')
+const port = process.env.PORT || 3000
 
 server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, 'views'))
@@ -11,5 +12,5 @@ server.use(express.static("public"))
 server.use(express.urlencoded({extended:true}))
 
 server.use(route)
-server.listen(3000, () => console.log("RODANDO"))
+server.listen(port, () => console.log("RODANDO"))
 
